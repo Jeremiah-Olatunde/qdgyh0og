50a99b52ca57397869ee29415b2fa97402b5f0c4
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-08-10 20:38:39 +0100

    chore(setup): init app

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+export function App() {
+  return (
+    <section>
+      <header>
+        <h1>Because I Could Not Stop For Death</h1>
+        <p>Emily Dickinson</p>
+      </header>
+
+      <p>
+        Because I could not stop for death He kindly stopped for me The carraige
+        held but just ourselves And immortality
+      </p>
+    </section>
+  );
+}
