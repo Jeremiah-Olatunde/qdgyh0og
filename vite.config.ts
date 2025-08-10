3e41c54b0e80f7a50dad7ea235135dd64f4184a5
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-08-10 21:34:22 +0100

    chore(setup): import aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
+import path from "node:path";
+import url from "node:url";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwind from "@tailwindcss/vite";
 
+const DIRECTORY = path.dirname(url.fileURLToPath(import.meta.url));
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind()],
+  resolve: {
+    alias: {
+      "@": path.resolve(DIRECTORY, "./src"),
+    },
+  },
 });
