cf67042b5679a797fb69af58f8402d93c6f1a1be
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-08-10 21:00:19 +0100

    chore(setup): font sora

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,7 @@ export function App() {
         <p>Emily Dickinson</p>
       </header>
 
-      <p>
+      <p className="text-red-500 font-sora">
         Because I could not stop for death He kindly stopped for me The carraige
         held but just ourselves And immortality
       </p>
