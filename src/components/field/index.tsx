f0d1a4541975254fe0ea1da8e2d484711bdcc3af
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-08-10 23:10:29 +0100

    feat(ui): component Field.Root

--- src/components/field/index.tsx
+++ src/components/field/index.tsx
@@ -1,8 +1,19 @@
 import { Field } from "@base-ui-components/react/field";
+import type { PropsWithChildren } from "react";
 
-type FieldLabelProps = Record<"htmlFor" | "text", string>;
+type RootProps = { name: string };
 
-export function Label({ htmlFor, text }: FieldLabelProps) {
+export function Root({ name, children }: PropsWithChildren<RootProps>) {
+  return (
+    <Field.Root name={name} className="flex flex-col gap-2">
+      {children}
+    </Field.Root>
+  );
+}
+
+type LabelProps = Record<"htmlFor" | "text", string>;
+
+export function Label({ htmlFor, text }: LabelProps) {
   return (
     <Field.Label htmlFor={htmlFor} className="justify-start flex items-center">
       <span className="font-sora text-xs font-medium capitalize">{text}</span>
