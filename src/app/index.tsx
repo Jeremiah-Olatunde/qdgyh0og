d8b5ef5c1b39688cdb4fa6131857983854dc0f60
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-08-10 21:07:54 +0100

    chore(setup): colors

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,7 @@ export function App() {
         <p>Emily Dickinson</p>
       </header>
 
-      <p className="text-red-500 font-sora">
+      <p className="text-primary font-sora">
         Because I could not stop for death He kindly stopped for me The carraige
         held but just ourselves And immortality
       </p>
